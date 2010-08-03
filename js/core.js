2b4746957546830f7bda75577cb9b78e11938f77
aloneroad <aloneroad@dv9505tx.AloneRoad>
2010-08-03 16:57:49 +0700

    clean code

--- js/core.js
+++ js/core.js
@@ -18,23 +18,30 @@ $j.fn.location = function() {
   })
 };
 
-var counter = {el:null, button:null, target:null, re:RegExp(/^\s*|\s*$/g), count:function() {
-  var a = counter.el.value.replace(/\n/g, ""), b = a.length;
-  chars_left = 256 - b;
-  if(chars_left >= 0) {
-    $j(counter.target.parentNode).is(".overlimit") && $j(counter.target.parentNode).removeClass("overlimit");
-    str = chars_left > 1 ? "c\u00f2n " + chars_left + " k\u00fd t\u1ef1" : chars_left > 0 ? "c\u00f2n 1 k\u00fd t\u1ef1" : "v\u1eeba h\u1ebft s\u1ed1 k\u00fd t\u1ef1 cho ph\u00e9p"
-  }else {
-    $j(counter.target.parentNode).is(".overlimit") || $j(counter.target.parentNode).addClass("overlimit");
-    str = chars_left < -1 ? "qu\u00e1 " + -chars_left + " k\u00fd t\u1ef1" : "qu\u00e1 1 k\u00fd t\u1ef1"
+var counter = {
+  el: null,
+  button: null,
+  target: null,
+  re: RegExp(/^\s*|\s*$/g),
+  count: function() {
+    var a = counter.el.value.replace(/\n/g, ""), b = a.length;
+    chars_left = 256 - b;
+    if (chars_left >= 0) {
+      $j(counter.target.parentNode).is(".overlimit") && $j(counter.target.parentNode).removeClass("overlimit");
+      str = chars_left > 1 ? "c\u00f2n " + chars_left + " k\u00fd t\u1ef1" : chars_left > 0 ? "c\u00f2n 1 k\u00fd t\u1ef1" : "v\u1eeba h\u1ebft s\u1ed1 k\u00fd t\u1ef1 cho ph\u00e9p"
+    }
+    else {
+      $j(counter.target.parentNode).is(".overlimit") || $j(counter.target.parentNode).addClass("overlimit");
+      str = chars_left < -1 ? "qu\u00e1 " + -chars_left + " k\u00fd t\u1ef1" : "qu\u00e1 1 k\u00fd t\u1ef1"
+    }
+    if ((b > 0 && b < 257 && a.replace(counter.re, "") != counter.el._value) == true) {
+      ob = $j(".send-message-disable");
+      ob.attr("onclick", "document.message_form.submit();");
+      ob.attr("class", "send-message-enable")
+    }
+    counter.target.nodeValue = str
   }
-  if((b > 0 && b < 257 && a.replace(counter.re, "") != counter.el._value) == true) {
-    ob = $j(".send-message-disable");
-    ob.attr("onclick", "document.message_form.submit();");
-    ob.attr("class", "send-message-enable")
-  }
-  counter.target.nodeValue = str
-}};
+};
 
 $j.fn.presence = function() {
   var a = $j("textarea#message", this), b = $j(this).find("input[@type=submit]"), c = null;
@@ -43,10 +50,14 @@ $j.fn.presence = function() {
   a.get(0)._value = a.attr("value");
   a.one("focus", function() {
     this.value = "";
-    $j(this).css({color:"#000"})
+    $j(this).css({
+      color: "#000"
+    })
   });
   a.focus(function() {
-    $j(this).css({color:"#000"});
+    $j(this).css({
+      color: "#000"
+    });
     counter.el = this;
     counter.button = b.get(0);
     counter.target = $j("p#counter").get(0).firstChild;
@@ -56,7 +67,7 @@ $j.fn.presence = function() {
     c && window.clearInterval(c)
   });
   this.submit(function() {
-    if(b.attr("disabled")) {
+    if (b.attr("disabled")) {
       b.show();
       $j("span.loader", this).hide();
       b.attr("disabled", true);
@@ -64,33 +75,42 @@ $j.fn.presence = function() {
     }
     var d = $j("input#loc");
     d.length > 0 && $j("input#location").attr("value", d.attr("value"));
-    if(window.location.search.indexOf("?page") != 0) {
-      $.ajax({type:"POST", url:this.action, data:$j("textarea, input, select", this).serialize(), success:function(e) {
-        var f = document.createElement("div");
-        f.innerHTML = e;
-        e = $j("li", f);
-        e.css("display", "none");
-        if(e.size() > 0) {
-          if($j("div#stream li.date:first").length) {
-            $j("div#stream li.date:first").after(e)
-          }else {
-            f = document.createElement("ul");
-            f.className = "stream";
-            $j("div#stream p:first").remove();
-            $j("div#stream").prepend(f);
-            $j("div#stream ul").append(e)
+    if (window.location.search.indexOf("?page") != 0) {
+      $.ajax({
+        type: "POST",
+        url: this.action,
+        data: $j("textarea, input, select", this).serialize(),
+        success: function(e) {
+          var f = document.createElement("div");
+          f.innerHTML = e;
+          e = $j("li", f);
+          e.css("display", "none");
+          if (e.size() > 0) {
+            if ($j("div#stream li.date:first").length) {
+              $j("div#stream li.date:first").after(e)
+            }
+            else {
+              f = document.createElement("ul");
+              f.className = "stream";
+              $j("div#stream p:first").remove();
+              $j("div#stream").prepend(f);
+              $j("div#stream ul").append(e)
+            }
+            e.toggle()
           }
-          e.toggle()
-        }else {
-          alert("Hmm. C\u00f3 m\u1ed9t s\u1ed1 th\u1ee9 \u0111\u00e3 ho\u1ea1t \u0111\u1ed9ng kh\u00f4ng \u0111\u00fang nh\u01b0 mong mu\u1ed1n :(")
+          else {
+            alert("Hmm. C\u00f3 m\u1ed9t s\u1ed1 th\u1ee9 \u0111\u00e3 ho\u1ea1t \u0111\u1ed9ng kh\u00f4ng \u0111\u00fang nh\u01b0 mong mu\u1ed1n :(")
+          }
+          b.show();
+          $j("span.loader", this).hide();
+          b.attr("disabled", true)
         }
-        b.show();
-        $j("span.loader", this).hide();
-        b.attr("disabled", true)
-      }});
+      });
       a.get(0)._value = a.attr("value");
       a.get(0).blur();
-      a.css({color:"#ccc"});
+      a.css({
+        color: "#ccc"
+      });
       return false
     }
   })
@@ -110,10 +130,11 @@ $j.fn.toggleable = function(a) {
   a = a;
   var b = this;
   $j("a[@href=#" + this.attr("id") + "]").click(function() {
-    if(b.css("display") == "none") {
+    if (b.css("display") == "none") {
       a && $j(a).hide();
       b.show()
-    }else {
+    }
+    else {
       a && $j(a).show();
       b.hide()
     }
@@ -131,10 +152,10 @@ $j.fn.toggleSelection = function(a, b) {
 $j.fn.icons = function() {
   var a = $j("a#add-icons", this), b = null, c = null, d = this;
   a.toggle(function() {
-    if(!b) {
+    if (!b) {
       var e = ['<div id="form-icons">'];
       $j("option", d).each(function() {
-        if(this.value != "") {
+        if (this.value != "") {
           e.push('<label for="icon-' + this.value + '" title="' + this.title + '">');
           e.push('<img src="' + this.id + '" class="icon" alt="' + this.text + '" />');
           this.title != "" && e.push("<h4> " + this.title + " </h4>");
@@ -152,7 +173,7 @@ $j.fn.icons = function() {
       $j("label", b).click(function() {
         var f = $j(this).attr("for").replace("icon-", ""), g = 0;
         $j("select#icon>option").each(function(j) {
-          if(this.value == f) {
+          if (this.value == f) {
             g = j
           }
         });
@@ -160,13 +181,17 @@ $j.fn.icons = function() {
         $j("label", b).removeClass("selected");
         $j(this).addClass("selected");
         c.attr("src", $j(this).find("img").get(0).src);
-        c.css({display:"inline"});
+        c.css({
+          display: "inline"
+        });
         c.click();
         var i = $j("textarea#message");
-        i.css({width:"349px"});
-        if(i.size() > 0) {
+        i.css({
+          width: "349px"
+        });
+        if (i.size() > 0) {
           d = i.get(0);
-          if(d._first) {
+          if (d._first) {
             d.value = this.title;
             d._first = null
           }
@@ -213,9 +238,13 @@ $j.fn.ajaxify = function() {
   this.click(function() {
     var a = $j(this).parent();
     a.html("vui l\u00f2ng ch\u1edd m\u1ed9t ch\u00fat...");
-    $.ajax({type:"GET", url:this.href, success:function(b) {
-      a.html(b)
-    }});
+    $.ajax({
+      type: "GET",
+      url: this.href,
+      success: function(b) {
+        a.html(b)
+      }
+    });
     return false
   })
 };
@@ -231,13 +260,14 @@ $j.fn.setAccordion = function() {
   a._current = null;
   b.click(function() {
     var c = this.hash.substring(1, this.hash.length);
-    if(a._current == c) {
+    if (a._current == c) {
       $j("div#" + a._current).removeClass("current");
       a.find("ul li").removeClass("current");
       a.removeClass("open");
       a._current = null;
       return false
-    }else {
+    }
+    else {
       a._current && $j("div#" + a._current).removeClass("current")
     }
     $j("div#" + c).addClass("current").find("input[@type='text'], input[@type='file'], textarea").get(0).focus();
@@ -263,15 +293,21 @@ $j.fn.toggleCheckbox = function() {
 $j.fn.poll = function(a) {
   var b = window.location.href;
   window.setInterval(function() {
-    $.ajax({type:"GET", url:b, dataType:"json", success:function(c) {
-      if(c.result) {
-        if(a) {
-          window.location.href = a
-        }else {
-          window.location.reload()
+    $.ajax({
+      type: "GET",
+      url: b,
+      dataType: "json",
+      success: function(c) {
+        if (c.result) {
+          if (a) {
+            window.location.href = a
+          }
+          else {
+            window.location.reload()
+          }
         }
       }
-    }})
+    })
   }, 2500)
 };
 
@@ -297,16 +333,17 @@ $j.fn.forms = function() {
     $j("span.loader", this).show()
   });
   this.each(function() {
-    switch(this.id) {
+    switch (this.id) {
       case "comment-form":
         $j("#participant-nicks > a", this).each(function() {
           $j(this).click(function() {
             var b = $j(this).text(), c = $j("#comment"), d = c.val(), e;
             c.focus();
-            if(c.get(0).selectionStart == undefined) {
+            if (c.get(0).selectionStart == undefined) {
               e = document.selection.createRange();
               e = Array(e.start, e.end)
-            }else {
+            }
+            else {
               e = Array(c.get(0).selectionStart, c.get(0).selectionEnd)
             }
             e[0] === 0 ? c.val("@" + b + ": " + d.substring(e[1], d.length)) : c.val(d.substring(0, e[0]) + "@" + b + d.substring(e[1], d.length));
@@ -334,7 +371,7 @@ $j.fn.forms = function() {
         break;
       case "form-avatar":
         $j("div.avatars", this).avatars();
-        if($j("a#toggle").size() > 0) {
+        if ($j("a#toggle").size() > 0) {
           $j("div.form-fields").hide();
           $j("a#toggle").click(function() {
             $j("div#account-form div.form-fields").show();
@@ -344,7 +381,7 @@ $j.fn.forms = function() {
         }
         break
     }
-    switch(this.parentNode.id) {
+    switch (this.parentNode.id) {
       case "form-message":
         $j(this).presence();
         break
@@ -355,17 +392,17 @@ $j.fn.forms = function() {
 $j(document).ready(function() {
   $j.browser.msie && $j("ul#main-nav>li").addHover();
   var a = document.body, b = $j(a).css("background-color");
-  if(!(b == "#ffffff" || b == "rbg(255,255,255)")) {
+  if (!(b == "#ffffff" || b == "rbg(255,255,255)")) {
     $j("form").forms();
     b = $j("div#sidebar");
-
+    
     $j("a.confirm-delete").confirm();
     $j("a.confirm-spam").confirm();
     $j("a.ajaxify").ajaxify();
     $j("div.tabs").setTabs();
     $j("div.accordion").setTabs();
     $j("form#change-number").toggleable("div#activation, div#activated");
-    if(a.id == "welcome" || a.id == "contacts") {
+    if (a.id == "welcome" || a.id == "contacts") {
       $j("a#select-all").toggleSelection("input[@name='actor[]'], input[@name='email[]']", true);
       $j("a#select-none").toggleSelection("input[@name='actor[]'], input[@name='email[]']", false)
     }
@@ -373,55 +410,58 @@ $j(document).ready(function() {
     $j("input#only-channel, input#only-user").toggleCheckbox()
   }
 });
-if(!Array.prototype.indexOf) {
+if (!Array.prototype.indexOf) {
   Array.prototype.indexOf = function(a, b) {
-    if(b == null) {
+    if (b == null) {
       b = 0
-    }else {
-      if(b < 0) {
+    }
+    else {
+      if (b < 0) {
         b = Math.max(0, this.length + b)
       }
     }
-    for(var c = b;c < this.length;c++) {
-      if(this[c] === a) {
+    for (var c = b; c < this.length; c++) {
+      if (this[c] === a) {
         return c
       }
     }
-    return-1
+    return -1
   }
 }
 var pwMinLen = 6, pwOkLen = 8, pwCut = 10;
 function passwordStrength(a, b) {
   var c = 0;
-  if(a.length > 0) {
-    if(a.length >= pwMinLen) {
+  if (a.length > 0) {
+    if (a.length >= pwMinLen) {
       a = a.substr(0, pwCut);
       var d = a.toLowerCase();
       c = 1;
-      for(var e = false, f = 0;f < b.length;f++) {
-        if(d.indexOf(b[f].substr(0, pwCut).toLowerCase()) != -1) {
+      for (var e = false, f = 0; f < b.length; f++) {
+        if (d.indexOf(b[f].substr(0, pwCut).toLowerCase()) != -1) {
           e = true;
           break
         }
       }
-      if(!e) {
+      if (!e) {
         a.length >= pwOkLen && c++;
         a.toLowerCase() != a && a.toUpperCase() != a && c++;
         a.search(/[0-9]/) != -1 && a.search(/[A-Za-z]/) != -1 && c++;
         a.search(/[^0-9A-Za-z]/) != -1 && c++;
-        if(c > 4) {
+        if (c > 4) {
           c = 4
         }
       }
     }
-  }else {
+  }
+  else {
     c = -1
   }
   return c
 }
+
 function analyseAccountPassword(a) {
   var b = document.getElementById("password").value, c = document.getElementById("pwstatus");
-  if(c || a) {
+  if (c || a) {
     ban = [];
     var d = document.getElementById("nick");
     d && d.value && ban.push(d.value);
@@ -430,40 +470,44 @@ function analyseAccountPassword(a) {
     (d = document.getElementById("city")) && d.value && ban.push(d.value);
     b = passwordStrength(b, ban);
     c.style.backgroundPosition = "0px " + (-b * 20 - 20) + "px";
-    if(a) {
+    if (a) {
       return b
     }
   }
 }
+
 function checkPassword(a) {
-  if(a.length < 8) {
+  if (a.length < 8) {
     return false
   }
-  if(a.search(/[A-Z]/) == -1) {
+  if (a.search(/[A-Z]/) == -1) {
     return false
   }
-  if(a.search(/[a-z]/) == -1) {
+  if (a.search(/[a-z]/) == -1) {
     return false
   }
-  if(a.search(/[0-9]/) == -1) {
+  if (a.search(/[0-9]/) == -1) {
     return false
   }
   return true
 }
+
 function getFieldValue(a) {
-  if((a = document.getElementById(a)) && a.value) {
+  if ((a = document.getElementById(a)) && a.value) {
     return a.value
   }
-  return""
+  return ""
 }
+
 function getOffset(a) {
-  for(var b = 0, c = 0;a.offsetParent;) {
+  for (var b = 0, c = 0; a.offsetParent;) {
     b += a.offsetTop || 0;
     c += a.offsetLeft || 0;
     a = a.offsetParent
   }
-  return[c, b]
+  return [c, b]
 }
+
 $j(function() {
   setTimeout(function() {
     $j("#notice").fadeOut(300)
@@ -471,25 +515,21 @@ $j(function() {
 });
 
 
-$j(document).ready(function($){
-	$j("body").fadeIn(500); 
-	
-    $j(document.body).click(function (event) {
-      if ((event.target.nodeName == "A" &&
-            event.target.className != "send-message-disable" &&
-            event.target.target != "_new" &&
-            event.target.target != "_blank"
-           ) || (
-        	 event.target.nodeName == "IMG" && 
-        	 event.target.className == "photo"
-           ) || (
-        	 event.target.type == "submit" && 
-        	 event.target.name != "submit-location"
-           ) || (
-             event.target.nodeName == "SPAN" && 
-             event.target.className != "nickname"
-           )) { 
-    	  $j(document.body).fadeOut(500) 
-    	  }
-      })
+$j(document).ready(function($) {
+  $j("body").fadeIn(500);
+  
+  $j(document.body).click(function(event) {
+    if ((event.target.nodeName == "A" &&
+    event.target.className != "send-message-disable" &&
+    event.target.target != "_new" &&
+    event.target.target != "_blank") ||
+    (event.target.nodeName == "IMG" &&
+    event.target.className == "photo") ||
+    (event.target.type == "submit" &&
+    event.target.name != "submit-location") ||
+    (event.target.nodeName == "SPAN" &&
+    event.target.className != "nickname")) {
+      $j(document.body).fadeOut(500)
+    }
   })
+})
